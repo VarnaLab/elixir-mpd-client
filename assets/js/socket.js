5f031510e73289f9d80e537439d6b3e3b95127d7
Vyacheslav macbook <vyachesalv.kara@gmail.com>
2018-05-07 20:51:00 +0300

    socket

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -54,9 +54,17 @@ let socket = new Socket("/socket", {params: {token: window.userToken}})
 socket.connect()
 
 // Now that you are connected, you can join channels with a topic:
-let channel = socket.channel("topic:subtopic", {})
+let channel = socket.channel("media_player:lobby", {})
 channel.join()
   .receive("ok", resp => { console.log("Joined successfully", resp) })
   .receive("error", resp => { console.log("Unable to join", resp) })
+  
+function action(param = "play"){
+    channel.push('action', { // send the message to the server on "shout" channel
+      name: "action",     // get value of "name" of person sending the message
+      message: param    // get message text (value) from msg input field.
+    });
+}
+action();
 
 export default socket
--- lib/elixir_mpd_client_web/channels/user_socket.ex
+++ lib/elixir_mpd_client_web/channels/user_socket.ex
@@ -2,7 +2,7 @@ defmodule ElixirMpdClientWeb.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", ElixirMpdClientWeb.RoomChannel
+   channel "media_player:lobby", ElixirMpdClientWeb.MediaPlayerChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
